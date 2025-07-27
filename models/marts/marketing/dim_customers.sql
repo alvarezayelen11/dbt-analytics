f9f1d5691a36ceea7df3cac7c8dbeb8cc0c66f29
Ayelen <alvarezayelen11@gmail.com>
2025-07-27 21:05:19 +0000

    Refactor dim_customers

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,12 +1,17 @@
 with customers as (
+
     select * from {{ ref('stg_jaffle_shop__customers') }}
+
 ),
 
 orders as (
+
     select * from {{ ref('stg_jaffle_shop__orders') }}
+
 ),
 
 customer_orders as (
+
     select
         customer_id,
         min(order_date) as first_order_date,
@@ -14,18 +19,40 @@ customer_orders as (
         count(order_id) as number_of_orders
     from orders
     group by 1
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_stripe__payments') }}
+
+),
+
+customer_lifetime as (
+
+    select
+        o.customer_id,
+        sum(p.amount) as lifetime_value
+    from payments p
+    join orders o on p.order_id = o.order_id
+    group by 1
+
 ),
 
 final as (
+
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-    from customers
-    left join customer_orders using (customer_id)
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders,
+        coalesce(cl.lifetime_value, 0) as lifetime_value
+    from customers c
+    left join customer_orders co using (customer_id)
+    left join customer_lifetime cl using (customer_id)
+
 )
 
 select * from final
